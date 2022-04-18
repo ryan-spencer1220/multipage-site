180da94cdde3490958b641ca22d31f0d1e99034d
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-18 11:57:21 -0700

    Complete React Router project

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,16 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Contact() {
+  const queryString = useLocation().search;
+  console.log(queryString);
+
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
+
   return (
     <div>
       <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
         excepturi facilis minus delectus obcaecati error, laboriosam molestiae
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,29 @@
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+// styles
+import "./Home.css";
+
 export default function Home() {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/articles");
+
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-        excepturi facilis minus delectus obcaecati error, laboriosam molestiae
-        nobis totam, amet qui quod non nostrum id rem exercitationem maiores,
-        repellat dolorem?
-      </p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>Written by {article.author}</p>
+            <Link to={`/articles/${article.id}`}>Read More...</Link>
+          </div>
+        ))}
     </div>
   );
 }
